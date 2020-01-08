18447dbd2cc1548b039c731d694fa97bd886e3c0
cheikh3008 <cheikh3008@gmail.com>
2020-01-08 08:09:44 +0000

    Création des fixtutes

--- samamoney/src/DataFixtures/AppFixtures.php
+++ samamoney/src/DataFixtures/AppFixtures.php
@@ -19,9 +19,9 @@ class AppFixtures extends Fixture
     }
     public function load(ObjectManager $manager)
     {
-        $role_admin_system = new Role();
-        $role_admin_system->setLibelle("ROLE_ADMIN_SYSTEM");
-        $manager->persist($role_admin_system);
+        $role_super_admin = new Role();
+        $role_super_admin->setLibelle("ROLE_SUPER_ADMIN");
+        $manager->persist($role_super_admin);
 
         $role_admin = new Role();
         $role_admin->setLibelle("ROLE_ADMIN");
@@ -31,11 +31,11 @@ class AppFixtures extends Fixture
         $role_caissier->setLibelle("ROLE_CAISSIER");
         $manager->persist($role_caissier);
 
-        $this->addReference('role_admin_system',$role_admin_system);
+        $this->addReference('role_super_admin',$role_super_admin);
         $this->addReference('role_admin',$role_admin);
         $this->addReference('role_caissier',$role_caissier);
         
-        $roleAdmdinSystem = $this->getReference('role_admin_system');
+        $roleAdmdinSystem = $this->getReference('role_super_admin');
         $roleAdmin = $this->getReference('role_admin');
         $roleAaissier = $this->getReference('role_caissier');
 
@@ -43,7 +43,7 @@ class AppFixtures extends Fixture
         $user->setEmail("cheikh3008@gmail.com");
         $user->setRole($roleAdmdinSystem);
         $user->setPassword($this->encoder->encodePassword($user, "admin123"));
-        $user->setRoles(json_encode(array("ROLE_ADMIN_SYSTEM")));
+        $user->setRoles(json_encode(array("ROLE_SUPER_ADMIN")));
         $user->setPrenom("Cheikh");
         $user->setNom("Dieng");
         $user->setStatus("active");
