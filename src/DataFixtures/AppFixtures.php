dcf0f6d3eebb480f8afd810613d987e469d8d167
cheikh3008 <cheikh3008@gmail.com>
2020-02-14 01:14:23 +0000

    Ajout des roles qui restent

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -18,34 +18,44 @@ class AppFixtures extends Fixture
         $this->encoder = $encoder;
     }
     public function load(ObjectManager $manager)
-    {
+
+    {   #### Ajout role admin systeme ######
         $role_super_admin = new Role();
         $role_super_admin->setLibelle("ROLE_SUPER_ADMIN");
         $manager->persist($role_super_admin);
 
+        #### Ajout role admin  ######
         $role_admin = new Role();
         $role_admin->setLibelle("ROLE_ADMIN");
         $manager->persist($role_admin);
 
+        #### Ajout role caissier  ######
         $role_caissier = new Role();
         $role_caissier->setLibelle("ROLE_CAISSIER");
         $manager->persist($role_caissier);
 
-        $this->addReference('role_super_admin',$role_super_admin);
-        $this->addReference('role_admin',$role_admin);
-        $this->addReference('role_caissier',$role_caissier);
+        #### Ajout role apartenaire ######
+        $role_partenaire = new Role();
+        $role_partenaire->setLibelle("ROLE_PARTENAIRE");
+        $manager->persist($role_partenaire);
         
-        $roleAdmdinSystem = $this->getReference('role_super_admin');
-        $roleAdmin = $this->getReference('role_admin');
-        $roleAaissier = $this->getReference('role_caissier');
+        #### Ajout role admin apartenaire ######
+        $role_admin_partenaire = new Role();
+        $role_admin_partenaire->setLibelle("ROLE_ADMIN_PARTENAIRE");
+        $manager->persist($role_admin_partenaire);
 
+        #### Ajout role apartenaire ######
+        $role_user_partenaire = new Role();
+        $role_user_partenaire->setLibelle("ROLE_USER_PARTENAIRE");
+        $manager->persist($role_user_partenaire);
+        
+        #### Ajout un admin system ######
         $user = new User();
         $user->setEmail("cheikh3008@gmail.com")
-            ->setRole($roleAdmdinSystem)
+            ->setRole($role_super_admin)
             ->setPassword($this->encoder->encodePassword( $user , "admin123"))
             ->setPrenom("Cheikh")
-            ->setNom("Dieng")
-            ->setIsActive(true);
+            ->setNom("Dieng");
         $manager->persist($user);
         $manager->flush();
     }
