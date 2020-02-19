b76d77d7d2d3e7061b34357749af7205f31e2c48
cheikh3008 <cheikh3008@gmail.com>
2020-02-19 23:50:08 +0000

    Fonction qui bloquer le partenaire et ses utlisatuers

--- src/Controller/BloquerPartenaireController.php
+++ src/Controller/BloquerPartenaireController.php
@@ -3,14 +3,15 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use App\Repository\PartenaireRepository;
 use App\Repository\RoleRepository;
 use App\Repository\UserRepository;
+use App\Repository\PartenaireRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 class BloquerPartenaireController extends AbstractController
@@ -22,23 +23,47 @@ class BloquerPartenaireController extends AbstractController
     }
     /**
      * @Route("/api/bloquer/{id}", name="bloquer_partenaire", methods={"PUT"})
+     * @IsGranted({"ROLE_ADMIN_SYSTEM" ,"ROLE_ADMIN"})
      */
    public function bloquer (Request $request, EntityManagerInterface $manager,PartenaireRepository $partenaireRepository, UserRepository $userRepository, RoleRepository $roleRepository,$id)
-   {
-        
-        $partenaire = $userRepository->find($id);
-        $res = $userRepository->findBy(array("partenaire" => $partenaire->getPartenaire()));
-        $count = count($res);
-        for ($i = 0; $i < $count; $i++){
-            if($partenaire->getPartenaire()){
-                $partenaire->setIsActive(false);
-                $manager->persist($partenaire);
-                $manager->flush();
+   {    
+        $values = json_decode($request->getContent());
+        $this->tokenStorage->getToken()->getUser();
+        ##### Récupération de l'ID du partenaire à bloquer #######
+        $partenaire_id = $userRepository->find($id);
+        ##### Récupération des utlisateurs du partenaire  #######
+        $partenaire = $userRepository->findBy(array("partenaire" => $partenaire_id->getPartenaire()));
+        ##### Bloquer le partenaire et es utlisateurs #######
+        if($partenaire_id->getIsActive() === true){
+            foreach ($partenaire as $result){
+                if($result->getPartenaire()){
+                    $result->setIsActive($values->isActive);
+                    $manager->persist($result);
+                    $manager->flush();
+                }
+            
             }
-        
+            $data = [
+                'status' => 201,
+                'message' => 'Le blocage du partenaires et ses utilisateurs a réussi avec sucess ...  ' 
+            ];
+
+            return new JsonResponse($data, 201);
+        }else{
+            foreach ($partenaire as $result){
+                if($result->getPartenaire()){
+                    $result->setIsActive($values->isActive);
+                    $manager->persist($result);
+                    $manager->flush();
+                }
+            
+            }
+            $data = [
+                'status' => 201,
+                'message' => 'Le déblocage du partenaires et ses utilisateurs a réussi avec sucess ...  ' 
+            ];
+
+            return new JsonResponse($data, 201);
         }
-        
-            return new JsonResponse("ok");
-         
-   }
+    }
 }
