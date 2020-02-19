325ce8fe7b40fb53cb3f44b02f8a2b9bf5f92c25
cheikh3008 <cheikh3008@gmail.com>
2020-02-19 23:51:44 +0000

    Vérification des valeurs à saisir , creation de la function qui gére la transaction, et mettre ajour le solde

--- src/Controller/EnvoiController.php
+++ src/Controller/EnvoiController.php
@@ -3,16 +3,16 @@
 namespace App\Controller;
 
 use App\Entity\Transaction;
+use App\Repository\TarifRepository;
 use App\Repository\CompteRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Repository\AffectationRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
-use App\Repository\TarifRepository;
-use App\Repository\TransactionRepository;
 
 class EnvoiController extends AbstractController
 {
@@ -25,62 +25,75 @@ class EnvoiController extends AbstractController
     }
     /**
      * @Route("/api/transaction/envoi", name="envoi", methods={"POST"})
+     * @IsGranted({"ROLE_PARTENAIRE" ,"ROLE_ADMIN_PARTENAIRE", "ROLE_USER_CAISSIER"})
      */
-    public function envoi(Request $request,EntityManagerInterface $manager, AffectationRepository $affectationRepository, CompteRepository $compteRipo, TarifRepository $tarifRepository)    {
+    public function envoi(Request $request,EntityManagerInterface $manager, AffectationRepository           $affectationRepository, CompteRepository $compteRipo, TarifRepository $tarifRepository)    {
         $userEnvoi = $this->tokenStorage->getToken()->getUser();
         $values = json_decode($request->getContent());
-        $userPartenaire = $userEnvoi->getPartenaire();
+
+        
         $userAffcete = $affectationRepository->findOneBy(array("user"=>$userEnvoi));
+        if($values)
+        {
+            #### Vérifie si l'utlisateur est affecté à un commpte ####
+            if($userAffcete)
+            {
+                $compte = $userAffcete->getCompte();
+                $compte = $compteRipo->findOneBy(array("id"=>$compte));
         
-        if($userAffcete){
-            $compte = $userAffcete->getCompte();
-            $compte = $compteRipo->findOneBy(array("id"=>$compte));
-    
-        }
-        elseif($compte = $compteRipo->findOneBy(array("numCompte" => $values->numCompte))){
-         if($values->numCompte != $userPartenaire){
-            $compte;
-        }   
-        }else{
-            $data = [
-                'status' => 500,
-                'message' => 'Aucun compte n\'est affecté à cet utlisateur . '];
-    
-            return new JsonResponse($data, 500);
-        }
+            }
+            #### Vérifie si l'utlisateur est partenaire ou admin partenaire ####
+            elseif($userEnvoi->getRole()->getLibelle() === "ROLE_PARTENAIRE" || $userEnvoi->getRole()->getLibelle() === "ROLE_ADMIN_PARTENAIRE" )
+            {
+                
+                $compte = $compteRipo->findOneBy(array("numCompte" => $values->numCompte));
+            
+            }
+            #### Vérifie si l'utlisateur n'est pas affacté à un compte ####
+            else
+            {
+                $data = [
+                    'status' => 500,
+                    'message' => 'Aucun compte n\'est affecté à cet utlisateur . '];
         
-        $tarif = $tarifRepository->findAll();
+                return new JsonResponse($data, 500);
+            }
+            
+            
+            #### Récupération du frais  ####
+         
+            $tarif = $tarifRepository->findAll();
         
             foreach ($tarif as $res)
             {
                 $res->getBorneInf();
                 $res->getBorneSup();
                 $res->getFrais();
-                if($values->montant >= $res->getBorneInf() && $values->montant <= $res->getBorneSUp() ){
+                if($values->montant >= $res->getBorneInf() && $values->montant <= $res->getBorneSup()){
                     $frais =  $res->getFrais();
 
                 }
                 
             }
-        if($values->montant > $compte->getSolde()){
+            #### Vérifie si le montant à envoyer est supérieur au solde du compte ####
+            if($values->montant > $compte->getSolde())
+            {
             $data = [
                 'status' => 201,
                 'message' => "Opération échouée, le solde est insuffisant ... "
             ];
     
                 return new JsonResponse($data, 201);
-        }
-        $comEtat = $frais * 30/100;
-        $comSysteme = $frais * 40/100 ;
-        $comEnvoi = $frais * 10/100 ;
-        $comRetrait = $frais * 20/100 ;
-        $code = rand(0,999999999) +1 ;
-            
-        $envoi = new Transaction();
-        $dateEnvoi = new \DateTime();
-         #### Faire un envoi ####
-        if($values){
-            
+            }
+            #### Calcul des parts pour chaque partie (etat, systeme, envoi, retrait) ####
+            $comEtat = $frais * 30/100;
+            $comSysteme = $frais * 40/100 ;
+            $comEnvoi = $frais * 10/100 ;
+            #### Génére un code  ####
+            $code = rand(0,999999999) +1 ;
+                
+            $envoi = new Transaction();
+            $dateEnvoi = new \DateTime();
             $envoi->setPrenomE($values->prenomE)
                     ->setnomE($values->nomE)
                     ->setTelephoneE($values->telephoneE)
@@ -106,17 +119,19 @@ class EnvoiController extends AbstractController
             $manager->persist($compte);
             $manager->flush();
             $data = [
-                'status' => 201,
-                'message' => 'Vous avez enoyé '.$values->montant. ' à '. $values->prenomE.' - '. $values->nomE .' - ' ];
-    
-                return new JsonResponse($data, 201);
-        }else{
+            'status' => 201,
+            'message' => 'Vous avez enoyé '.$values->montant. ' à '. $values->prenomE.' - '. $values->nomE .' - ' ];
+
+            return new JsonResponse($data, 201);
+        }
+        else
+        {
             $data = [
                 'status' => 500,
                 'message' => 'Veuillez saisir tous les champs ... '
                 ] ;
     
-                return new JsonResponse($data, 500);
+            return new JsonResponse($data, 500);
         }
        
     }
